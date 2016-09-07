c92a8b584b67376e747d625b35f242f9f400dc4b
dance2die <dontbothermewithspam@gmail.com>
2016-09-07 06:53:16 -0400

    Learned about adding/removing values from a list

---
+++ Level1/ListDemo.py
@@ -0,0 +1,18 @@
+food = ['Banana', 'Kiwi', 'Cookies']
+print(food)
+
+food.append('Udon')
+food.append('Gyudon')
+print(food)
+
+del food[2:3]
+print(food)
+
+food.remove('Banana')
+print(food)
+
+
+
+
+
+
