50516e52bbffb823b0ccb7e818fb4ff52644c6c5
dance2die <dontbothermewithspam@gmail.com>
2016-09-07 07:04:37 -0400

    Learned about Python Dictionary

---
+++ Level1/DictionaryDemo.py
@@ -0,0 +1,18 @@
+
+map = {"Sung": "Wizard", "Michael": "Dwarf", "Dennis": "Performer"}
+map["Lama"] = "animal"
+print(map)
+
+del map["Sung"]
+print(map)
+
+map["Michael"] = "eater"
+print(map)
+
+
+
+
+
+
+
+
